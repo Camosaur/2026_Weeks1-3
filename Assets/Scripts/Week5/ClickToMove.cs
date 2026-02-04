f45063c56b6cdacd7b45a187addc2098fe4e9e7c
CAMERON\camer <cameronpletsch@gmail.com>
2026-02-04 10:03:27 -0500

    Made a duck move when you click but not when you click the buttons

---
+++ Assets/Scripts/Week5/ClickToMove.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
+public class ClickToMove : MonoBehaviour
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame == true && !EventSystem.current.IsPointerOverGameObject()) { 
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            transform.position = mousePos;
+        }
+        
+    }
+}
